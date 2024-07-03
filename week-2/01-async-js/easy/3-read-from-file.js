dd0bc9d354de8871da25cba1bb94e0b6ba6b5c52
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-03 16:24:59 +0530

    read from file

--- week-2/01-async-js/easy/3-read-from-file.js
+++ week-2/01-async-js/easy/3-read-from-file.js
@@ -4,3 +4,24 @@
 // You can use the fs library to as a black box, the goal is to understand async tasks.
 // Try to do an expensive operation below the file read and see how it affects the output.
 // Make the expensive operation more and more expensive and see how it affects the output.
+
+const fs = require("fs");
+
+fs.readFile("a.txt", "utf-8", function (err, data) {
+  console.log(data);
+});
+
+console.log("heyy");
+
+setTimeout(() => {
+  console.log("inside set timeout");
+}, 5000);
+
+let a = 0;
+for (let i = 0; i < 10000; i++) {
+  a = a + i;
+}
+
+console.log("outside");
+
+console.log(a);
