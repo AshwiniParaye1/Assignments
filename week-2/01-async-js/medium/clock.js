5e613f2d8b285658dfda059262eb9a6678eb6760
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-03 18:07:22 +0530

    rename function

--- week-2/01-async-js/medium/clock.js
+++ week-2/01-async-js/medium/clock.js
@@ -1,8 +1,8 @@
-function counter() {
+function clock() {
   setInterval(function () {
     let currentTime = new Date();
     console.log(currentTime.toLocaleTimeString());
   }, 1000);
 }
 
-counter();
+clock();
